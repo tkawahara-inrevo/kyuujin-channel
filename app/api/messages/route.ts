e3508cd29dfeebc3e32db2562acc7ea79e3f0e07
tkawahara-inrevo <t.kawahara@inrevo.jp>
2026-01-31 19:53:54 +0900

    step2 修正版re

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -12,8 +12,10 @@ function getString(v: unknown): string {
 }
 
 function isUuid(s: string): boolean {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(s);
+  // UUIDの形式(8-4-4-4-12)だけ確認。version/variantは見ない
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(s);
 }
+
 
 type ApplicationRow = {
   id: string;
